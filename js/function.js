1e3325dea74123491155485fe8dde3d8022ac0d6
kensakugioia <kensakugioia@gmail.com>
2023-07-02 17:39:11 +0900

    アロー関数　うまくでない

---
+++ js/function.js
@@ -0,0 +1,18 @@
+// 朝の挨拶を出力する関数を定義する
+const sayGoodMorning = () => {
+    console.log('おはようございます!');
+    console.log('昨日はよく眠れましたか?');
+    console.log('今日も1日頑張りましょう!');
+}
+ 
+//夜の挨拶を出力する関すを定義する
+const sayGoodEvening = () => {
+    console.log('こんばんは！');
+    console.timeLog('今日も1日お疲れ様でした');
+}
+
+// 朝の挨拶を出力する関数選び出す
+sayGoodMorning();
+
+// 夜の挨拶を出力する関数を呼び出す
+sayGoodEvening();
